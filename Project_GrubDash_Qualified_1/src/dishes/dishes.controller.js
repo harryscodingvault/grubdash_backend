11978c84c8eda44ad94b0f4ed6e111cf7d918a33
HaroldDRF <95robots@gmail.com>
2022-04-18 13:40:31 -0700

    post dishes routed added

--- Project_GrubDash_Qualified_1/src/dishes/dishes.controller.js
+++ Project_GrubDash_Qualified_1/src/dishes/dishes.controller.js
@@ -12,6 +12,47 @@ const list = (req, res) => {
   res.json({ data: dishes });
 };
 
+const bodyHasProperty = (propertyName) => {
+  return (req, res, next) => {
+    const { data = {} } = req.body;
+    if (data[propertyName] && data[propertyName] !== "") {
+      if (propertyName === "price") {
+        if (!Number.isInteger(data[propertyName]) || data[propertyName] <= 0) {
+          next({
+            status: 400,
+            message: `Dish must have a price that is an integer greater than 0`,
+          });
+        }
+      }
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Dish must include a ${propertyName}`,
+    });
+  };
+};
+
+const createDish = (req, res, next) => {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name: name,
+    description: description,
+    price: price,
+    image_url: image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+};
+
 module.exports = {
   list,
+  createDish: [
+    bodyHasProperty("name"),
+    bodyHasProperty("description"),
+    bodyHasProperty("price"),
+    bodyHasProperty("image_url"),
+    createDish,
+  ],
 };
--- Project_GrubDash_Qualified_1/src/dishes/dishes.router.js
+++ Project_GrubDash_Qualified_1/src/dishes/dishes.router.js
@@ -3,6 +3,10 @@ const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
-router.route("/").get(controller.list).all(methodNotAllowed);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.createDish)
+  .all(methodNotAllowed);
 
 module.exports = router;
